fc0c19d48674bd0be73b4f0f0a7385a8dabcfd55
mootez-bahri <mootez.bahri@esprit.tn>
2025-10-21 03:40:28 -0700

    Prosit2

--- src/Zoo.java
+++ src/Zoo.java
@@ -19,36 +19,7 @@ public class Zoo {
                 " / Number of cages: " + this.nbrCages +
                 " / Number of animals: " + this.nbrAnimaux);
     }
-    public int searchAnimal(Animal animal)
-    {
-        for(int i=0;i<nbrAnimaux;i++)
-        {
-            if(animal.name==animaux[i].name)
-                return i;
-        }
-        return -1;
-    }
-    public void addAnimal(Animal animal)
-    {
-        if(searchAnimal(animal)!=-1) {
-            if(nbrAnimaux==nbrCages) {
-                animaux[nbrAnimaux] = animal;
-                nbrAnimaux++;
-            }
-        }
 
-    }
-    public void removeAnimal(Animal animal) {
-        int index = searchAnimal(animal);
-        if (index == -1) {
-            for (int i = index; i < nbrAnimaux - 1; i++) {
-                animaux[i] = animaux[i + 1];
-            }
-
-            animaux[nbrAnimaux - 1] = null;
-            nbrAnimaux--;
-        }
-    }
 
     @Override
     public String toString() {
